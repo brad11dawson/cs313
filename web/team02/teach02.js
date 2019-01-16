10a67d4962a26ffb9a0eb95af10e18fb07a3c369
Bradley Dawson <brad11dawson@gmail.com>
2019-01-16 12:15:33 -0700

    added a change background color feature

--- web/team02/teach02.js
+++ web/team02/teach02.js
@@ -1,3 +1,8 @@
 function clickFunction() {
   alert("Clicked!");
 }
+
+function changeColor() {
+  var newColor = document.getElementById("newColor").value;
+  document.getElementById("one").style.backgroundColor = newColor;
+}
