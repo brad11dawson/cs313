a3a96adaa15cff967cfc25f9907cad7f1b550075
Bradley Dawson <brad11dawson@gmail.com>
2019-01-16 20:56:46 -0700

    added feature to hide 3rd section and implemented jquery

--- web/team02/teach02.js
+++ web/team02/teach02.js
@@ -2,7 +2,25 @@ function clickFunction() {
   alert("Clicked!");
 }
 
+//change color with regular js
 function changeColor() {
   var newColor = document.getElementById("newColor").value;
   document.getElementById("one").style.backgroundColor = newColor;
 }
+
+//change color with jquery
+$(document).ready(function () {
+  $("#colorButton").click(function () {
+    var color = $("#newColor").val();
+    $("#one").css("background-color", color);
+  });
+  
+  $("#hideButton").click(function () {
+    $("#three").hide(1000);
+  });
+  
+  $("#showButton").click(function () {
+    $("#three").show(1000);
+  });
+});
+
