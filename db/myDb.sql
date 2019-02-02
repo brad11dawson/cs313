22baa0722b313a7d8e57d41b04d9097c991bb12a
Bradley Dawson <brad11dawson@gmail.com>
2019-02-01 22:15:07 -0700

    added a file with commands to create database

---
+++ db/myDb.sql
@@ -0,0 +1,58 @@
+# database has already been created in heroku
+
+# Users information will have a username and password to sign in.
+# It will also have a display name for others to see when 
+# they make a review
+CREATE TABLE public.user
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL,
+	display_name VARCHAR(100) NOT NULL
+);
+
+# The game will have a name and a description
+# it will be crossrefrenced with its genre and 
+# consoles in a many to many table 
+CREATE TABLE public.game
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	game_name VARCHAR(100) NOT NULL,  
+	description TEXT NOT NULL,
+);
+
+# A review will contain a score and a review(description).
+# It will also have a refrence to what game it reviews, and
+# the user who gave the review
+CREATE TABLE public.review
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	score INT NOT NULL,
+	user_id INT NOT NULL REFERENCES public.user(id),
+	game_id INT NOT NULL REFERENCES public.game(id),
+	description TEXT NOT NULL,
+);
+
+CREATE Table public.genre
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	genre_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE public.console
+(
+	id SERIAL NOT NULL PRIMARY KEY,
+	console_name VARCHAR(100) NOT NULL
+)
+
+CREATE Table public.game_console
+(
+	game_id INT NOT NULL REFERENCES public.game(id),
+	console_id INT NOT NULL REFERENCES public.console(id)
+);
+
+CREATE TABLE public.game_genre
+(
+	game_id INT NOT NULL REFERENCES public.game(id),
+	console_id INT NOT NULL REFERENCES public.console(id)
+);
