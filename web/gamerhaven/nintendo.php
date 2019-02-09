145ca10f939e263c5f5487c593a3eabf7d0372f7
Bradley Dawson <brad11dawson@gmail.com>
2019-02-09 15:23:49 -0700

    Added database queries for playstation and nintendo games

--- web/gamerhaven/nintendo.php
+++ web/gamerhaven/nintendo.php
@@ -12,6 +12,42 @@
   
 <body>
   <?php include 'header.php'; ?>
-  <p>This is for nintendo games</p>
+  <h2>Nintendo Switch</h2>
+  
+  <?php
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+  
+  $statement = $db->query("SELECT game_name, description 
+  FROM game_console 
+  INNER JOIN game ON game_console.game_id = game.id 
+  INNER JOIN console ON game_console.console_id = console.id 
+  WHERE console_name = 'Nintendo Switch';");
+  
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+  {
+    echo '<h2>' . $row['game_name'] . '</h2>';
+    echo '<p>' . $row['description'] . '</p>';
+  }
+  ?>
 </body>
 </html>
