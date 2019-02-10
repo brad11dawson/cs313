cb037ca00d5ab47838214387a7232506248a487b
Bradley Dawson <brad11dawson@gmail.com>
2019-02-09 19:29:21 -0700

    test passing game name in url parameters

--- web/gamerhaven/showgames.php
+++ web/gamerhaven/showgames.php
@@ -4,8 +4,8 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   echo '<div class="container bg-light my-2 py-2">';
   echo '<h3>' . $row['game_name'] . '</h3>';
   echo '<p>' . $row['description'] . '</p>';
-  $game_name = $row['game_name'];
-  echo '<a href="gamereviews.php" class="btn btn-primary btn-md active" role="button" aria-disabled="true">See Reviews</a>';
+  $urlstring = 'gamereviews.php?gamename=' + $row['game_name'];
+  echo '<a href=$urlstring class="btn btn-primary btn-md active" role="button" aria-disabled="true">See Reviews</a>';
   echo '</div>';
 }
 ?>
