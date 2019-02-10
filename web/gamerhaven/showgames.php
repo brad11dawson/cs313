a72c42597901bbba569f4efd754a747911f2b724
Bradley Dawson <brad11dawson@gmail.com>
2019-02-09 19:35:27 -0700

    test 3

--- web/gamerhaven/showgames.php
+++ web/gamerhaven/showgames.php
@@ -5,7 +5,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   echo '<h3>' . $row['game_name'] . '</h3>';
   echo '<p>' . $row['description'] . '</p>';
   $urlstring = 'gamereviews.php?gamename=' + $row['game_name'];
-  echo '<a href="$urlstring" class="btn btn-primary btn-md active" role="button" aria-disabled="true">See Reviews</a>';
+  echo '<a href=' . $urlstring . 'class="btn btn-primary btn-md active" role="button" aria-disabled="true">See Reviews</a>';
   echo '</div>';
 }
 ?>
