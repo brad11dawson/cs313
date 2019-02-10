638df2f456bbf0f0fbe3f16853bf2abc1e997c5b
Bradley Dawson <brad11dawson@gmail.com>
2019-02-09 19:08:22 -0700

    added a tiny margin between games

--- web/gamerhaven/showgames.php
+++ web/gamerhaven/showgames.php
@@ -1,7 +1,7 @@
 <?php
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-  echo '<div class="container bg-light">';
+  echo '<div class="container bg-light my-2">';
   echo '<h3>' . $row['game_name'] . '</h3>';
   echo '<p>' . $row['description'] . '</p>';
   echo '<a href="#" class="btn btn-primary btn-md active" role="button" aria-disabled="true">See Reviews</a>';
