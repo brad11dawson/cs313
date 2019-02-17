7bd73574ce5efdff88ca763febb1cdfa350fd340
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 21:48:26 -0700

    test post

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -5,4 +5,7 @@ $score = $_POST['score'];
 $review_content = $_Post['review_content'];
 
 echo "<h2>" . $game_name . "<h2>";
+echo "<h2>" . $user_id . "<h2>";
+echo "<h2>" . $score . "<h2>";
+echo "<h3>" . $review_content . "<h3>";
 ?>
