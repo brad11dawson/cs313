e42071f261f54f2c20a4eb551b9a9f589daa9e6d
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 22:08:37 -0700

    added special chars

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -2,7 +2,7 @@
 $game_name = $_POST['game_name'];
 $user_id = $_POST['user_id'];
 $score = $_POST['score'];
-$review_content = $_POST['review_content'];
+$review_content = htmlspecialchars($_POST['review_content']);
 
 echo "<h2>" . $game_name . "</h2>";
 echo "<h2>" . $user_id . "</h2>";
