e2ef6e24a4f4e5772de75e7eb9c2a159d7908eea
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 22:05:18 -0700

    fixed POST declaration

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -1,8 +1,8 @@
 <?php
 $game_name = $_POST['game_name'];
-$user_id = $_Post['user_id'];
+$user_id = $_POST['user_id'];
 $score = $_POST['score'];
-$review_content = htmlspecialchar($_Post['review_content']);
+$review_content = htmlspecialchar($_POST['review_content']);
 
 echo "<h2>" . $game_name . "</h2>";
 echo "<h2>" . $user_id . "</h2>";
