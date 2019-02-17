062c6ccde85e5727c67a307ea1a91bc7a75b246a
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 21:55:41 -0700

    fixed header tags

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -4,8 +4,8 @@ $user_id = $_Post['user_id'];
 $score = $_POST['score'];
 $review_content = $_Post['review_content'];
 
-echo "<h2>" . $game_name . "<h2>";
-echo "<h2>" . $user_id . "<h2>";
-echo "<h2>" . $score . "<h2>";
-echo "<h3>" . $review_content . "<h3>";
+echo "<h2>" . $game_name . "</h2>";
+echo "<h2>" . $user_id . "</h2>";
+echo "<h2>" . $score . "</h2>";
+echo "<h3>" . $review_content . "</h3>";
 ?>
