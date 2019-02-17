53f20f74385651b7ee019eb85c07a39e91663bf8
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 22:00:21 -0700

    added specialchars to handle input

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -2,7 +2,7 @@
 $game_name = $_POST['game_name'];
 $user_id = $_Post['user_id'];
 $score = $_POST['score'];
-$review_content = $_Post['review_content'];
+$review_content = htmlspecialchar($_Post['review_content']);
 
 echo "<h2>" . $game_name . "</h2>";
 echo "<h2>" . $user_id . "</h2>";
