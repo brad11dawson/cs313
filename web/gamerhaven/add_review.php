bb685b48ffa6b6eb4a0154f47186de96c1ee5eef
Bradley Dawson <brad11dawson@gmail.com>
2019-02-17 00:36:05 -0700

    This might work

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -10,9 +10,11 @@ $statement = $db->query("SELECT id FROM game WHERE game_name = '$game_name'");
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 $game_id = $result[0]["id"];
 
-$stmt = $db->prepare('INSERT INTO review(game_id, user_id, score, description) VALUES ($game_id, $user_id, :score, ":review_content");');
+$stmt = $db->prepare('INSERT INTO review(game_id, user_id, score, description) VALUES (:$game_id, :$user_id, :score, ":review_content");');
 $stmt->bindValue(':review_content', $review_content, PDO::PARAM_STR);
 $stmt->bindValue(':score', $score, PDO::PARAM_INT);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+$stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
 $stmt->execute();
 
 $new_page = "gamereviews.php?gamename=$game_name";
