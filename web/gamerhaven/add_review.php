818f7fee3e0d1f0e784ae5a2032c893d150b4145
Bradley Dawson <brad11dawson@gmail.com>
2019-02-17 00:42:34 -0700

    trying to get user updates to work

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -10,7 +10,7 @@ $statement = $db->query("SELECT id FROM game WHERE game_name = '$game_name'");
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 $game_id = $result[0]["id"];
 
-$stmt = $db->prepare('INSERT INTO review(game_id, user_id, score, description) VALUES (:$game_id, :$user_id, :score, ":review_content");');
+$stmt = $db->prepare('INSERT INTO review(game_id, user_id, score, description) VALUES (:$game_id, :$user_id, :score, ':review_content');');
 $stmt->bindValue(':review_content', $review_content, PDO::PARAM_STR);
 $stmt->bindValue(':score', $score, PDO::PARAM_INT);
 $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
