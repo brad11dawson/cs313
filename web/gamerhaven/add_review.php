189e0befcb2cf44ef2943f5a5a2f06a6592d1c1f
Bradley Dawson <brad11dawson@gmail.com>
2019-02-16 22:39:22 -0700

    test 3

--- web/gamerhaven/add_review.php
+++ web/gamerhaven/add_review.php
@@ -6,17 +6,9 @@ $review_content = htmlspecialchars($_POST['review_content']);
 
 require('connectdatabase.php');
 
-$statement = $db->query('SELECT id FROM game WHERE game_name = $game_name');
+$statement = $db->query("SELECT id FROM game WHERE game_name = '$game_name'");
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 $game_id = $result['id'];
 echo "<h1>game id: $game_id </h1>";
 
-/*$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-*/
-
-//INSERT INTO review(score, user_id, game_id, description) 
 ?>
